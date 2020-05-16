9c2d8da6a31e0a32f45e345ecca21435b31e4ece
thedinindu <thedinindu@gmail.com>
2020-05-17 01:43:45 +0530

    Updates

---
+++ precache-manifest.5e16809ea615876dfef5e6b143a2a143.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f70b9585f13da4c6b71dee720f27868",
+    "url": "/Covid-19-Tracker-App/index.html"
+  },
+  {
+    "revision": "67eab6f6ed0bc05f30d7",
+    "url": "/Covid-19-Tracker-App/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "0b28ce7882c0e86c7c77",
+    "url": "/Covid-19-Tracker-App/static/css/main.e5361b36.chunk.css"
+  },
+  {
+    "revision": "67eab6f6ed0bc05f30d7",
+    "url": "/Covid-19-Tracker-App/static/js/2.573b1ad3.chunk.js"
+  },
+  {
+    "revision": "1fd4b1d005d715d5cbab4fdcc335be90",
+    "url": "/Covid-19-Tracker-App/static/js/2.573b1ad3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b28ce7882c0e86c7c77",
+    "url": "/Covid-19-Tracker-App/static/js/main.5ac9c51c.chunk.js"
+  },
+  {
+    "revision": "0ad08076edccba9ff4b9",
+    "url": "/Covid-19-Tracker-App/static/js/runtime-main.af4bd3fd.js"
+  }
+]);
